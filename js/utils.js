733c0febeed58056d1b702eebbcb7a33b2eab64c
Minseok0917 <nminseok0917@naver.com>
2022-02-03 22:19:00 +0900

    drag시 충돌 element middleY 충돌 시 위 아래 변경 utils 작성

--- js/utils.js
+++ js/utils.js
@@ -8,8 +8,10 @@ export const elementRect = (element) => {
 	const {left, top, width, height} = element.getBoundingClientRect();
 	const middleX = left + width/2;
 	const middleY = top + height/2;
+	const idx = element.dataset.idx;
 	return {
 		element,
+		idx,
 		left,
 		top,
 		width,
@@ -19,16 +21,37 @@ export const elementRect = (element) => {
 	}
 };
 
-export const isClash = (focusOption,refOption) => {
+
+export const isClash = (focusOption,refOption,isClash) => {
 	const {x, y} = getPosDistance(focusOption,refOption);
+	console.log(refOption.element, isClash)
 	const isX = focusOption.width/2 > x;
-	const isY = refOption.height/2+focusOption.height/2 > y;
+	const isY = refOption.height/2+focusOption.height/2+(isClash ? focusOption.height : 0 ) > y;
 	return isX && isY;	
 };
-export const isClashTop = (focusOption,refOption) => {
+export const isClashMiddleY = (focusOption,refOption) => {
+	const focusTop = focusOption.middleY-focusOption.height/2;
 	const focusBottom = focusOption.middleY+focusOption.height/2;
-	return focusBottom < refOption.middleY;
+
+	const top = Math.abs(refOption.middleY-focusTop);
+	const bottom = Math.abs(refOption.middleY-focusBottom);
+
+	// console.log(top,bottom);
+
+	return false;
+	// console.log(top,bottom);
+	// return ( top < 5 ) || ( bottom < 5);
 };
+
+
+export const isClashFocusTop = (focusOption,refOption) => {
+	const focusTop = focusOption.middleY-focusOption.height/2;
+	return focusTop < refOption.middleY;
+};
+export const isClashFocusBottom = (focusOption,refOption)=>{
+	const focusBottom = focusOption.middleY+focusOption.height/2;
+	return focusBottom > refOption.middleY && focusBottom-refOption.middleY < 5;
+}
 
 export const getPosDistance = function(focusOption,refOption){
 	const x = Math.abs(focusOption.middleX-refOption.middleX);
