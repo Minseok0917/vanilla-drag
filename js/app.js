4afd95bfc3a9826193fa96c8ed4ef250b617250f
Minseok0917 <nminseok0917@naver.com>
2022-02-07 23:38:04 +0900

    app.js 최적화

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 import useDrag from './store/drag.js';
+import useTodo from './store/todo.js';
+import render from './render.js';
 import {
 	queryAll
 } from './utils.js';
@@ -10,6 +12,7 @@ import {
 } from './event.js';
 
 
+/*
 const $itemContainer = queryAll('.item-container');
 const $items = queryAll('.item-container .item');
 
@@ -19,15 +22,24 @@ const $itemContainerAddEvent = ($itemContainer) => {
 const $itemAddEvent = ($item) => {
 	$item.addEventListener('mousedown',$itemMouseDown);
 };
+*/
 
-function initial(){
+
+async function initial(){
+	const { todos } = await fetch('todo.json').then( res => res.json() );
+	useTodo.commit('setTodos',todos);
+
 	window.addEventListener('mousemove',mouseMove); // drag, 충돌, 충돌 + 거리 비례 스크롤 감지
 	window.addEventListener('mouseup',mouseUp);
+
+	render();
+	/*
 	$itemContainer.forEach($itemContainerAddEvent);
 	$items.forEach($itemAddEvent);
 	$items.forEach( ($item,idx) => $item.setAttribute('data-idx',idx) );
-	
 	useDrag.commit('setItems',$items);
+*/
+
 }
 
 initial();
