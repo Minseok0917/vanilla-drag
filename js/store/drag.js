293544960d3bd6f9614e03b673c830c4b86befd2
Minseok0917 <nminseok0917@naver.com>
2022-02-09 23:54:31 +0900

    Container 추가

--- js/store/drag.js
+++ js/store/drag.js
@@ -2,6 +2,7 @@ import defineStore from '../plugin/store.js';
 
 const defaultState = () => ({
 	isDown:false,
+	$itemContainer:[],
 	$items:[],
 	$focusItem:{},
 	focusOption:{},
@@ -13,6 +14,7 @@ const state = defaultState();
 const getters = {
 	isDown:(state)=>(state.isDown),
 	$items:(state)=>(state.$items),
+	$itemContainers:(state)=>(state.$itemContainers),
 	$focusItem:(state)=>(state.$focusItem),
 	focusOption:(state)=>(state.focusOption),
 	clashIdx:(state)=>(state.clashIdx),
@@ -20,6 +22,7 @@ const getters = {
 const mutations = {
 	setIsDown:(state,payload)=>(state.isDown=payload),
 	setItems:(state,payload)=>(state.$items=payload),
+	setItemContainers:(state,payload)=>(state.$itemContainers=payload),
 	setFocusItem:(state,payload)=>(state.$focusItem=payload),
 	setFocusOption:(state,payload)=>(state.focusOption=payload),
 	setClashIdx:(state,paylaod)=>(state.clashIdx=paylaod)
