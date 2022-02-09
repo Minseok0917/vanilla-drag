06149cc3acfb6714919411c191a3d51e27b1271c
Minseok0917 <nminseok0917@naver.com>
2022-02-09 23:53:43 +0900

    item 화면 render module 작성

---
+++ js/render.js
@@ -0,0 +1,51 @@
+import useDrag from './store/drag.js';
+import useTodo from './store/todo.js';
+import { queryAll } from './utils/dom.js';
+import { $itemMouseDown } from './event.js';
+
+
+
+const $itemContainers = queryAll('.todo-container .item-container');
+const [
+	$todoContainer,
+	$processContaienr,
+	$closeContainer
+] = $itemContainers;
+
+
+
+const convertToElement = ({idx,name}) => (`
+	<div class="item" data-idx="${idx}">
+		<p class="title">${name}</p>
+	</div>
+`);
+const lastElement = (`<div class="item"></div>`);
+const renderItem = () => {
+	const {todos} = useTodo.getters;
+	$itemContainers.forEach( $itemContainer => {
+		const type = $itemContainer.dataset.type;
+		const itemHTML = todos[type].map(convertToElement).join('')+lastElement;
+		$itemContainer.innerHTML = itemHTML;
+	});
+}
+
+
+const itemEvent = ($item) => {
+	$item.addEventListener('mousedown',$itemMouseDown);
+};
+{ 
+	const itemContainerEvent = ($itemContainer) => {
+		// $itemContainer.addEventListener('')
+	};
+	$itemContainers.forEach(itemContainerEvent);
+	useDrag.commit('setItemContainers',$itemContainers);
+}
+
+
+export default function(){
+	renderItem();
+	const $items = queryAll('.item-container .item');
+	$items.forEach(itemEvent);
+
+	useDrag.commit('setItems',$items);
+}
