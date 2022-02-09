a64691bdcb861a0d184375bb316177c4bd71fedf
Minseok0917 <nminseok0917@naver.com>
2022-02-09 23:54:04 +0900

    event 추가 & localStorage ?? getTodo 셋팅

--- js/app.js
+++ js/app.js
@@ -1,32 +1,20 @@
 import useStorage from './plugin/storage.js';
+import getTodo from './api/getTodo.js';
+import { setTodos } from './utils/todo.js';
+import {
+	globalMouseMove,
+	globalMouseUp
+} from './event.js';
 
-/*
-	localStorage 에 Todo 데이터가 없으면 
-	todo.json 에서 데이터를 가져오고 가공 후 Localstorage 에 집어넣기로
-*/
 
-const storageName = 'todos';
-
-async function init(){
-	const todos = useStorage.get(storageName) ?? getJSON('./todo.json',function({todos}){
-		const reduceInitValue = {
-			todo:[],
-			process:[],
-			close:[]
-		};
-		todos.reduce((acc,todo)=>({
-			...acc,
-			[todo.type]: [...acc[todo.type],todo]
-		}),reduceInitValue);
-		useStorage.set(storageName,todos);
-		return todos;
-	});
+function event(){
+	window.addEventListener('mousemove',globalMouseMove);
+	window.addEventListener('mouseup',globalMouseUp);
 }
-
-async function getJSON(path,callback){
-	return await fetch(path)
-		.then( res => res.json() )
-		.then(callback)
+async function init(){
+	const todos = useStorage.get('todos') ?? await getTodo();
+	setTodos(todos);
+	event();
 }
 
 init();
